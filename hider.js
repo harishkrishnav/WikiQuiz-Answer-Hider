1e471eaf241bc3a717d08e1a3ac8dc9577ff8a58
Harish <hakr@microsoft.com>
2021-06-27 09:47:38 +0530

    Added files

---
+++ hider.js
@@ -0,0 +1,54 @@
+hideAnswers();
+
+async function hideAnswers() {
+  console.log("We are in WikiQuiz!");
+  const delay = ms => new Promise(res => setTimeout(res, ms));
+  await delay(3000);
+
+  try{
+    var qandatab = document.getElementById("qanda-tab");
+    qandatab.click();  
+  }
+  catch(e){
+    ;
+  }
+  
+  var elements = document.getElementsByClassName("ng-binding");
+  let counter = 0;
+
+  for (item of elements) {
+    try {
+      if(item.attributes['data-ng-bind-html'].value === "r.answer|render"){
+        item.id = counter;
+        counter = counter+1;
+
+        item.style.visibility = 'hidden';
+        
+        const answerCopy = document.createElement("p");
+        answerCopy.className = counter;
+        answerCopy.innerText = item.innerText;
+        answerCopy.style.display = 'none';
+        answerCopy.style.width = 'fit-content';
+
+        const newButton = document.createElement("button");
+        newButton.className = counter;
+        newButton.innerText = "Click to reveal answer";
+        newButton.style.visibility = 'visible';
+        newButton.style.width = 'stretch';
+        newButton.onclick = () => {
+          newButton.style.display = 'none'; 
+          answerCopy.style.display = 'block'; 
+          answerCopy.style.visibility = 'visible'; 
+          item.style.display = 'none';
+        };
+
+        item.prepend(newButton);
+        item.prepend(answerCopy);
+
+      }
+    }
+    catch(e) {
+      ;
+    }
+  }
+}
